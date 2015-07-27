87dc3fce3865c38280a2619f95e03b148d1572aa
infofreakz <infofreakzzz@gmail.com>
2015-07-27 11:26:00 +0800

    EasyLearningPHP
    EasyLearningPHP

--- LessonFiles/chapter2-comments/3unixstylecomment.php
+++ LessonFiles/chapter2-comments/3unixstylecomment.php
@@ -4,17 +4,3 @@
 # It effects for single line
 echo "<center>"."Hello World!"."<br />"."This PHP Script uses the Unix Style comment"."</center>";
 ?>
-
-<html>
-<head>
-    <title>
-        This is the title
-    </title>
-</head>
-<body>
-        <div style="width: 50px; height: 100px; margin-top: 100px; margin-left: 100px; float: left; background-color: aqua;">Yoyo</div>
-        <div style="width: 50px; height: 100px; margin-top: 100px; margin-left: 10px; float: right; background-color: aqua;">Yaya</div>
-       
-
-</body>
-</html>
