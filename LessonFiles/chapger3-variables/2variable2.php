61596859919e4b527ec618229a3ad35af1b2843d
infofreakz <infofreakzzz@gmail.com>
2015-07-28 11:11:56 +0800

    EasyLearningPHP
    EasyLearningPHP

---
+++ LessonFiles/chapger3-variables/2variable2.php
@@ -0,0 +1,6 @@
+<?php
+$var1 = "Hello";
+$var2 = "I'm";
+$var3 = "NulledByte";
+echo $var1,"\t",$var2,"\t",$var3;
+?>
---
+++ LessonFiles/chapger3-variables/3variable3.php
@@ -0,0 +1,10 @@
+<?php
+$var1 = "Hello";
+$var2 = "I'm";
+$var3 = "NulledByte and I'm a Programmer";
+$var4 = "I love Programming";
+$var5 = "My age is";
+$age = 31;
+$var6 = "years old";
+echo "<center>".$var1."\t".$var2."\t".$var3."\t".$var4."<br />".$var5."\t".$age."\t".$var6."</center>";
+?>
