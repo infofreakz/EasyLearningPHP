e79c3f3699420866e4b015605a308ec505092e17
infofreakz <infofreakzzz@gmail.com>
2015-07-29 15:49:22 +0800

    EasyLearningPHP
    EasyLearningPHP

---
+++ LessonFiles/chapter4-strings-basic/1string1.php
@@ -0,0 +1,9 @@
+<?php
+/*
+This is showing the basic of strings
+*/
+$greeting = "Hello World";
+$name = "I'm NulledByte";
+$occupation = "I'm a PHP Programmer";
+echo "<center>".$greeting."<br />".$name."<br />".$occupation."</center>";
+?>
