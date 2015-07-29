a3ed5a50196c31579e3b1607d006162cd39c7403
infofreakz <infofreakzzz@gmail.com>
2015-07-29 16:41:29 +0800

    EasyLearningPHP
    EasyLearningPHP

---
+++ LessonFiles/chapter5-integers-basic/3inaddition2.php
@@ -0,0 +1,7 @@
+<?php
+/*
+In this example, we gonna add the values directly without using the variables
+ဒီဥပမာမှာတော့ variable တွေကို မသုံးတော့ပဲ တန်ဖိုးတွေကို တိုက်ရိုက် ပေါင်းပြထားပါတယ်။
+*/
+echo "When adding 2 & 3, the result is"."&nbsp".(2+3);
+?>
