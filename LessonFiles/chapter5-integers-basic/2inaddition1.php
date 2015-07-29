e7fc954578ea7725f508ecf408363bf9dab57f86
infofreakz <infofreakzzz@gmail.com>
2015-07-29 16:29:33 +0800

    EasyLearningPHP
    EasyLearningPHP

---
+++ LessonFiles/chapter5-integers-basic/2inaddition1.php
@@ -0,0 +1,10 @@
+<?php
+/*
+In this example, we gonna add 2 variables directly
+ဒီဥပမာမှာတော့ $var1 နဲ့ $var2 တို့ကို ပေါင်းပြထားပါတယ်။
+အဲဒီတော့ $var1 ရဲ့တန်ဖိုးသည် 2 ဖြစ်ပြီး $var2 ရဲ့တန်ဖိုးသည် 3 ဖြစ်တဲ့အတွက်ကြောင့် variable ၂ခုရဲ့ ပေါင်းလဒ်ဖြစ်တဲ့ 5 ဆိုတဲ့တန်ဖိုးကို ဖော်ပြပေးပါလိမ့်မယ်။
+*/
+$var1 = 2;
+$var2 = 3;
+echo "When adding $var1 & $var2, the result is"."\t".($var1+$var2);
+?>
